Fixing up scripts. Added precision script. Minor refactors

src/helper/data.py
@@ -1,13 +1,14 @@
-from mlxtend.data import loadlocal_mnist
-from mlxtend.preprocessing import one_hot
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import math
 import random
 import copy
+from keras.datasets import mnist,cifar10
+from globals import EPSILON
 
 def get_unique_examples(train_x, train_y, N):
+  # When working with minimal data, get as representative examples as possible
   ex_per_class = math.ceil(N / 10)
   indices = []
   for i, ex in enumerate(train_x):
@@ -17,11 +18,15 @@ def get_unique_examples(train_x, train_y, N):
   return indices[0:N]
 
 def normalize(matrix, row=False):
-  precision = 3
+  # Have one less precision than global minimum comparison
+  precision = int(np.log10(EPSILON)+1)*-1
   if row:
+    # Normalize each column
     matrix = (matrix - np.min(matrix, axis=0)) / (np.max(matrix, axis=0) - np.min(matrix, axis=0))
   else:
+    # Normalize every element in matrix
     matrix = (matrix - np.min(matrix)) / (np.max(matrix) - np.min(matrix))
+  # Round to precision so sign calculations are easier
   matrix = np.around(matrix, decimals=precision)
   return matrix
 
@@ -33,13 +38,45 @@ def oh_encode(labels):
     encoded[i][int(val)] = 1
   return encoded
 
+def get_architecture(data, hls):
+  """data is dict with train/test/val data. hls is list of size of hidden layers"""
+  in_neurons = [data["train_x"].shape[1]]
+  out_neurons = [data["oh_train_y"].shape[1]]
+
+  return in_neurons + hls + out_neurons
+
+def get_batches(data, batch_size):
+  batches = []
+  for i in range(0, len(data["train_x"]), batch_size):
+    tmp_data = copy.deepcopy(data)
+    tmp_data["train_x"] = data["train_x"][i:i+batch_size]
+    tmp_data["train_y"] = data["train_y"][i:i+batch_size]
+    tmp_data["oh_train_y"] = data["oh_train_y"][i:i+batch_size]
+
+    batches.append(tmp_data)
+  
+  return batches
+
 def load_data(dataset, N, seed):
+  """Return list of batches of dataset. If batch_size == 0 : all N elements are in first batch"""
   random.seed(seed)
+  if dataset == "mnist":
+    data = load_keras(mnist, N)
+  elif dataset == "cifar":
+    data = load_keras(cifar10, N)
+  elif dataset == "adult":
+    data = load_adult(N)
+  elif dataset == "heart":
+    data = load_heart(N)
+  else:
+    raise Exception("Dataset %s not known" % dataset)
+
+  return data
+
+def load_keras(dataset, N):
 
   (train_x, train_y), (test_x, test_y) = dataset.load_data()
-  # Normalize
-  #train_x = train_x/255
-  #test_x = test_x/255
+  
   train_x = normalize(train_x)
   test_x = normalize(test_x)
 
@@ -48,9 +85,6 @@ def load_data(dataset, N, seed):
   test_x = test_x.reshape(-1, pixels)
 
   train_indices = [x for x in range(len(train_x))]
-
-  if seed:
-    random.shuffle(train_indices)
 
   train_x = train_x[train_indices]
   train_y = train_y[train_indices]
@@ -74,8 +108,7 @@ def load_data(dataset, N, seed):
 
   return data
 
-def load_heart(N, seed):
-  random.seed(seed)
+def load_heart(N):
 
   heart = pd.read_csv('data/heart.csv')
   cp = pd.get_dummies(heart['cp'], prefix='cp')
@@ -121,8 +154,7 @@ def load_heart(N, seed):
 
   return data
 
-def load_adult(N, seed):
-  random.seed(seed)
+def load_adult(N):
 
   columns = ['age', 'workclass', 'fnlwgt', 'education', 'education.num', 'marital.status',
              'occupation', 'relationship', 'race', 'sex', 'capital.gain', 'capital.loss',
@@ -180,20 +212,6 @@ def load_adult(N, seed):
   data["oh_test_y"]= oh_encode(test_y)
 
   return data
-
-
-def get_batches(data, batch_size):
-  batches = []
-  for i in range(0, len(data["train_x"]), batch_size):
-    tmp_data = copy.deepcopy(data)
-    tmp_data["train_x"] = data["train_x"][i:i+batch_size]
-    tmp_data["train_y"] = data["train_y"][i:i+batch_size]
-    tmp_data["oh_train_y"] = data["oh_train_y"][i:i+batch_size]
-
-    batches.append(tmp_data)
-  
-  return batches
-
 
 def imshow(example):
   plt.imshow(example.reshape([28, 28]))

src/main_run.py
@@ -4,11 +4,10 @@ from milp.min_w import MIN_W
 from milp.max_correct import MAX_CORRECT
 from milp.min_hinge import MIN_HINGE
 from milp.min_hinge_reg import MIN_HINGE_REG
-from helper.misc import inference, calc_accuracy,clear_print
-from helper.data import load_data, get_batches, load_heart, load_adult
+from helper.misc import inference, calc_accuracy, infer_and_accuracy, clear_print, get_bound_matrix,get_mean_vars
+from helper.data import load_data, get_batches, get_architecture
 from helper.save_data import DataSaver
 import argparse
-from keras.datasets import mnist,cifar10
 import numpy as np
 from pdb import set_trace
 
@@ -17,13 +16,6 @@ milps = {
   "max_correct": MAX_CORRECT,
   "min_hinge": MIN_HINGE,
   "min_hinge_reg": MIN_HINGE_REG
-}
-
-datasets = {
-  "mnist": mnist,
-  "cifar10": cifar10,
-  "heart": None,
-  "adult": None,
 }
 
 solvers = {
@@ -36,7 +28,7 @@ if __name__ == '__main__':
   parser = argparse.ArgumentParser(description='Optional app description')
 
   parser.add_argument('--solver', default="gurobi", type=str)
-  parser.add_argument('--hl', default='16', type=str)
+  parser.add_argument('--hls', default='16', type=str)
   parser.add_argument('--ex', default=10, type=int)
   parser.add_argument('--focus', default=0, type=int)
   parser.add_argument('--time', default=1, type=float)
@@ -46,10 +38,12 @@ if __name__ == '__main__':
   parser.add_argument('--bound', default=1, type=int)
   parser.add_argument('--batch', default=0, type=int)
   parser.add_argument('--save', action='store_true', help="An optional flag to save data")
+  parser.add_argument('--all', action='store_true', help="An optional flag to run on all data")
+  parser.add_argument('--mean', action='store_true', help="An optional flag to use mean variables")
   args = parser.parse_args()
   
   solver = args.solver
-  hl = [int(x) for x in args.hl.split("-")]
+  hls = [int(x) for x in args.hls.split("-")]
 
   N = args.ex
   focus = args.focus
@@ -65,42 +59,29 @@ if __name__ == '__main__':
   if loss not in milps:
     raise Exception("MILP model %s not known" % loss)
 
-  if data not in datasets:
-    raise Exception("Dataset %s not known" % data)
-
   if solver not in solvers:
     raise Exception("Solver %s not known" % solver)
 
+  data = load_data(data, N, seed)
 
-  # Load data (MNIST/CIFAR10)
-  if data == 'heart':
-    data = load_heart(N, seed)
-  elif data == 'adult':
-    data = load_adult(N, seed)
-  else:
-    data = load_data(datasets[data], N, seed)
-  batches = [data]
-
-  if batch_size > 0 and batch_size < N:
-    batches = get_batches(data, batch_size)
-  else:
+  if batch_size == 0:
     batch_size = N
+  batches = get_batches(data, batch_size)
 
-  in_neurons = [data["train_x"].shape[1]]
-  out_neurons = [data["oh_train_y"].shape[1]]
+  architecture = get_architecture(data, hls)
+  print("architecture", architecture)
 
-  # Set up NN layers, including input size, hidden layer sizes and output size
-  architecture = in_neurons + hl + out_neurons
   print_str = "Architecture: %s. N: %s. Solver: %s. Loss: %s. Bound: %s"
-  clear_print(print_str % ("-".join([str(x) for x in architecture]), len(data["train_x"]), solver, loss, bound))
+  clear_print(print_str % ("-".join([str(x) for x in architecture]), N, solver, loss, bound))
 
   get_nn = solvers[solver]
   networks = []
   network_vars = []
 
+  ### RUN BATCHES
   batch_num = 0
   for batch in batches:
-    clear_print("Batch: %s. Examples: %s-%s" % (batch_num+1, batch_num*batch_size, (batch_num+1)*batch_size))
+    clear_print("Batch: %s. Examples: %s-%s" % (batch_num, batch_num*batch_size, (batch_num+1)*batch_size))
     nn = get_nn(milps[loss], batch, architecture, bound)
     nn.train(train_time*60, focus)
 
@@ -109,10 +90,8 @@ if __name__ == '__main__':
 
     varMatrices = nn.extract_values()
 
-    infer_train = inference(nn.data["train_x"], varMatrices, nn.architecture)
-    infer_test = inference(nn.data["test_x"], varMatrices, nn.architecture)
-    train_acc = calc_accuracy(infer_train, nn.data["train_y"])
-    test_acc = calc_accuracy(infer_test, nn.data["test_y"])
+    train_acc = infer_and_accuracy(nn.data['train_x'], nn.data["train_y"], varMatrices, nn.architecture)
+    test_acc = infer_and_accuracy(nn.data['test_x'], nn.data["test_y"], varMatrices, nn.architecture)
 
     print("Training accuracy: %s " % (train_acc))
     print("Testing accuracy: %s " % (test_acc))
@@ -122,29 +101,14 @@ if __name__ == '__main__':
 
     batch_num += 1
 
+
+  ### RUN ALL TOGETHER FROM BATCHES WARM START
   if batch_size > 0 and batch_size != N:
     clear_print("Using warm start from batches")
-    all_vars = network_vars[0]
-    all_vars = {}
-    mean_vars = {}
-    bound_matrix = {}
-    num_batches = len(network_vars)
-    for key in network_vars[0]:
-      all_vars[key] = np.stack([tmp[key] for tmp in network_vars])
-      mean_vars[key] = np.mean(all_vars[key], axis=0)
-      mean_vars[key][mean_vars[key] < 0] -= 1e-5
-      mean_vars[key][mean_vars[key] >= 0] += 1e-5
-      mean_vars[key] = np.round(mean_vars[key])
-      if "w_" in key or "b_" in key:
-        tmp_min = np.min(all_vars[key],axis=0)
-        tmp_max = np.max(all_vars[key],axis=0)
-        tmp_min[tmp_min > 0] = -bound
-        tmp_max[tmp_max < 0] = bound
-        bound_matrix["%s_%s" % (key,"lb")] = tmp_min
-        bound_matrix["%s_%s" % (key,"ub")] = tmp_max
+    bound_matrix = get_bound_matrix(network_vars, bound)
 
     warm_nn = get_nn(milps[loss], data, architecture, bound)
-    #warm_nn.warm_start(mean_vars)
+    #warm_nn.warm_start(get_mean_vars(network_vars))
     warm_nn.update_bounds(bound_matrix)
     warm_nn.train(train_time*60, focus)
 
@@ -153,14 +117,18 @@ if __name__ == '__main__':
 
     varMatrices = warm_nn.extract_values()
 
-    infer_train = inference(warm_nn.data["train_x"], varMatrices, warm_nn.architecture)
-    infer_test = inference(warm_nn.data["test_x"], varMatrices, warm_nn.architecture)
-    train_acc = calc_accuracy(infer_train, warm_nn.data["train_y"])
-    warm_test_acc = calc_accuracy(infer_test, warm_nn.data["test_y"])
+    warm_train_acc = infer_and_accuracy(warm_nn.data['train_x'], warm_nn.data["train_y"], varMatrices, warm_nn.architecture)
+    warm_test_acc = infer_and_accuracy(warm_nn.data['test_x'], warm_nn.data["test_y"], varMatrices, warm_nn.architecture)
 
-    print("Training accuracy: %s " % (train_acc))
+    print("Training accuracy: %s " % (warm_train_acc))
     print("Testing accuracy: %s " % (warm_test_acc))
 
+    warm_nn_time = sum([i.get_runtime() for i in networks]) + warm_nn.get_runtime()
+    clear_print("Warm start run time: %.2f. Accuracy: %s" % (warm_nn_time, warm_test_acc))
+
+
+  ### RUN ALL DATA AT ONCE
+  if args.all:
     clear_print("All data at once")
     nn = get_nn(milps[loss], data, architecture, bound)
     nn.train(train_time*60, focus)
@@ -170,23 +138,21 @@ if __name__ == '__main__':
 
     varMatrices = nn.extract_values()
 
-    infer_train = inference(nn.data["train_x"], varMatrices, nn.architecture)
-    infer_test = inference(nn.data["test_x"], varMatrices, nn.architecture)
-    train_acc = calc_accuracy(infer_train, nn.data["train_y"])
-    test_acc = calc_accuracy(infer_test, nn.data["test_y"])
+    train_acc = infer_and_accuracy(nn.data['train_x'], nn.data["train_y"], varMatrices, nn.architecture)
+    test_acc = infer_and_accuracy(nn.data['test_x'], nn.data["test_y"], varMatrices, nn.architecture)
 
     print("Training accuracy: %s " % (train_acc))
     print("Testing accuracy: %s " % (test_acc))
 
-    warm_nn_time = sum([i.get_runtime() for i in networks]) + warm_nn.get_runtime()
 
-    clear_print("Warm start run time: %.2f. Accuracy: %s" % (warm_nn_time, warm_test_acc))
     clear_print("All at once run time: %.2f. Accuracy: %s" % (nn.get_runtime(), test_acc))
 
-    infer_train = inference(nn.data["train_x"], mean_vars, nn.architecture)
-    infer_test = inference(nn.data["test_x"], mean_vars, nn.architecture)
-    train_acc = calc_accuracy(infer_train, nn.data["train_y"])
-    test_acc = calc_accuracy(infer_test, nn.data["test_y"])
+  ### TAKE MEAN VARIABLES AND RUN INFERENCE
+  if args.mean:
+    mean_vars = get_mean_vars(network_vars)
+
+    train_acc = infer_and_accuracy(nn.data['train_x'], nn.data["train_y"], mean_vars, nn.architecture)
+    test_acc = infer_and_accuracy(nn.data['test_x'], nn.data["test_y"], mean_vars, nn.architecture)
 
     clear_print("Training accuracy for mean parameters: %s" % (train_acc))
     clear_print("Testing accuracy for mean parameters: %s" % (test_acc))

src/milp/gurobi_nn.py
@@ -78,7 +78,7 @@ def get_gurobi_nn(NN, data, architecture, bound):
       for index, count in np.ndenumerate(maybe_var):
         try:
           # Sometimes solvers have "integer" values like 1.000000019, round it to 1
-          if maybe_var[index].VType == 'I':
+          if maybe_var[index].VType in ['I', 'B']:
             tmp[index] = round(maybe_var[index].x)
           else:
             tmp[index] = maybe_var[index].x

src/scripts/compare_precision_heart.py
@@ -11,13 +11,12 @@ from milp.max_correct import MAX_CORRECT
 from milp.min_hinge import MIN_HINGE
 from milp.min_hinge_reg import MIN_HINGE_REG
 
-
 num_examples = [50, 100, 150, 200]
+bounds = [1, 3, 7, 15]
 
 time = 2*60
 seeds = [1,2,3]
 hl_neurons = 20
-bound = 1
 
 milps = {
   "min_w": MIN_W,
@@ -26,85 +25,85 @@ milps = {
   "min_hinge_reg": MIN_HINGE_REG
 }
 
-short = False
+short = True
 if short:
-  num_examples = num_examples[0:2]
-  seeds = [4,8]
+  num_examples = [40, 60, 80]
+  bounds = [1,3,7]
+  seeds = [10,20]
   time = 2
 
-def compare_heart_one_hl(losses, plot=False):
+def compare_precision_heart(losses, plot=False):
+  if len(losses) > 1:
+    raise Exception("This experiment is meant for only one loss")
+  loss = losses[0]
 
   all_results = {}
-  num_experiments = len(losses)
 
-  clear_print("Starting script, max time left: %s minutes" % get_time_left(1,1, num_experiments))
+  clear_print("Starting script, max time left: %s minutes" % get_time_left(0,0))
 
-  i = 0
-  json_dir = "results/json/compare_heart_one_hl"
+  json_dir = "results/json/compare_precision_heart"
   pathlib.Path(json_dir).mkdir(exist_ok=True)
+  file_name = "%s-Time:%s-HL_Neurons:%s-Loss:%s-S:%s-Prec:%s" % (loss, time, hl_neurons, loss, len(seeds), len(bounds))
+  json_path = "%s/%s.json" % (json_dir, file_name)
+  if pathlib.Path(json_path).is_file():
+    print("Path %s exists" % json_path)
+    with open(json_path, "r") as f:
+      data = json.loads(f.read())
+      all_results = data["results"]
+  else:
+    i = 0
+    for bound in bounds:
+      print("Running %s nn experiments for bound %s!" % (loss, bound))
+      i += 1
+      all_results[str(bound)] = run_experiments(loss, bound)
 
-  for loss in losses:
-    i += 1
-    print("Running %s nn experiments!" % loss)
-    file_name = "%s-Time:%s-HL_Neurons:%s-Bound:%s-S:%s" % (loss, time, hl_neurons, bound, len(seeds))
-    json_path = "%s/%s.json" % (json_dir, file_name)
-    if pathlib.Path(json_path).is_file():
-      print("Path %s exists" % json_path)
-      with open(json_path, "r") as f:
-        data = json.loads(f.read())
-        all_results[loss] = data["results"]
-      continue
-
-    if loss in milps:
-      all_results[loss] = run_experiments(loss, i, num_experiments)
-    else:
-      print("Loss %s unknown" % loss)
-      continue
     with open(json_path, "w") as f:
-      data = {"results": all_results[loss], "ts": datetime.now().strftime("%d-%m-%H:%M")}
+      data = {"results": all_results, "ts": datetime.now().strftime("%d-%m-%H:%M")}
       json.dump(data, f)
 
   x = num_examples
   plt.figure(1)
-  for loss in losses:
-    y, err = get_acc_mean_std(all_results[loss])
-    plt.errorbar(x, y, yerr=err, capsize=1, label="%s test performance" % loss)
+  for bound in bounds:
+    y, err = get_acc_mean_std(all_results[str(bound)])
+    plt.errorbar(x, y, yerr=err, capsize=1, label="Test performance for precision: %s" % bound)
   plt.legend()
   plt.xlabel("Number of examples")
   plt.ylabel("Test performance %")
-  plt.title("Compare test accuracies for Heart Dataset with bound %s" % bound)
-  plot_dir = "results/plots/compare_heart_one_hl/performance"
+  plt.title("Compare test accuracies for loss %s on Heart Dataset for different precisions" % loss)
+  plot_dir = "results/plots/compare_precision_heart/performance"
   pathlib.Path(plot_dir).mkdir(parents=True, exist_ok=True)
   title = "%s_TS:%s" % (file_name, datetime.now().strftime("%d-%m-%H:%M"))
   if plot:
     plt.savefig("%s/%s.png" % (plot_dir,title),  bbox_inches='tight')
 
   plt.figure(2)
-  for loss in losses:
-    y, err = get_runtime_mean_std(all_results[loss])
-    plt.errorbar(x, y, yerr=err, capsize=1, label="%s runtime" % loss)
+  for bound in bounds:
+    y, err = get_runtime_mean_std(all_results[str(bound)])
+    plt.errorbar(x, y, yerr=err, capsize=1, label="Runtime for precision: %s" % bound)
   plt.legend()
   plt.xlabel("Number of examples")
   plt.ylabel("Runtime [s]")
-  plt.title("Compare runtimes for Heart Dataset with bound %s" % bound)
-  plot_dir = "results/plots/compare_heart_one_hl/runtimes"
+  plt.title("Compare runtimes for loss %s on Heart Dataset for different precisions" % loss)
+  plot_dir = "results/plots/compare_precision_heart/runtimes"
   pathlib.Path(plot_dir).mkdir(parents=True, exist_ok=True)
   if plot:
     plt.savefig("%s/%s.png" % (plot_dir,title),  bbox_inches='tight')
 
-def run_experiments(loss, experiment, num_experiments):
+def run_experiments(loss, bound):
   nn_results = []
   i = 0
   for N in num_examples:
-    i += 1
     accs = []
     runtimes = []
-    clear_print("Max time left: %s" % get_time_left(i, experiment, num_experiments))
+    clear_print("Max time left: %s" % get_time_left(i, bounds.index(bound)))
     for s in seeds:
       data = load_data("heart", N, s)
       arch = get_architecture(data, [hl_neurons])
+      in_neurons = [data["train_x"].shape[1]]
+      out_neurons = [data["oh_train_y"].shape[1]]
+      arch = in_neurons + [hl_neurons] + out_neurons
 
-      clear_print("%s:  HL_Neurons: %s, N: %s, Seed: %s" % (loss, hl_neurons, N, s))
+      clear_print("%s:  HL_Neurons: %s, N: %s, Seed: %s, Bound: %s" % (loss, hl_neurons, N, s, bound))
       nn = get_gurobi_nn(milps[loss], data, arch, bound)
       nn.train(60*time, 0)
       obj = nn.get_objective()
@@ -117,6 +116,7 @@ def run_experiments(loss, experiment, num_experiments):
       runtimes.append(runtime)
 
     nn_results.append((accs, runtimes))
+    i += 1
 
   return nn_results
 
@@ -131,9 +131,9 @@ def get_runtime_mean_std(results):
   std = [np.std(z[1]) for z in results]
   return mean, std
 
-def get_time_left(example, experiment, num_experiments):
-  time_left = len(num_examples[example-1:])*time*len(seeds)
-  time_left += len(num_examples)*time*len(seeds)*(num_experiments - experiment)
+def get_time_left(example, bound_index):
+  time_left = len(num_examples[example:])*time*len(seeds)
+  time_left += len(num_examples)*time*len(seeds)*(len(bounds) - bound_index - 1)
 
   days = time_left // (60*24)
   time_left -= days*60*24